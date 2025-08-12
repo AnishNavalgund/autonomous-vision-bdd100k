299e125184fa1bf46879791150ff2382f32fe3fc
AnishNavalgund <anishk.navalgund@gmail.com>
2025-08-13 01:16:36 +0200

    update notebooks

--- scripts/gpu_check.py
+++ scripts/gpu_check.py
@@ -9,6 +9,9 @@ def main():
     if cuda:
         device_count = torch.cuda.device_count()
         print(f"Number of GPUs: {device_count}")
+        print(
+            "Allocated memory:", torch.cuda.memory_allocated(0) / 1024**2, "MB"
+        )
     else:
         print("No GPU")
 
